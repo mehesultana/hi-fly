d22e8440f919c4b9b8133bb586854f43746dcccf
mehesultana <sultanamehe01@gmail.com>
2022-05-09 00:13:16 +0600

    setup

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+export const Navbar = () => {
+	return (
+		<div className="navbar">
+			<div className="navcontainer">
+				<span className="logo">Hi Fly</span>
+			</div>
+		</div>
+	);
+};
