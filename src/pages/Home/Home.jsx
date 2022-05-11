fdf885ff5f101a6a33e6263ea1fc06a32a1deffe
mehesultana <sultanamehe01@gmail.com>
2022-05-11 21:14:09 +0600

    Email List Component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Featured } from '../../components/Featured/Featured';
 import { FeaturedProperties } from '../../components/FeaturedProperties/FeaturedProperties';
 import { Header } from '../../components/Header/Header';
+import { MailList } from '../../components/MailList/MailList';
 import { Navbar } from '../../components/Navbar/Navbar';
 import { PropertyList } from '../../components/PropertyList/PropertyList';
 import './Home.css';
@@ -17,6 +18,7 @@ export const Home = () => {
 				<PropertyList />
 				<h1 className="homeTitle">Homes guests love</h1>
 				<FeaturedProperties />
+				<MailList />
 			</div>
 		</div>
 	);
