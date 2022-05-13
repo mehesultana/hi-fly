22154348d4c733a986911c7571e2190205e3b363
mehesultana <sultanamehe01@gmail.com>
2022-05-14 00:21:16 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { List } from './pages/List/List';
 import { Hotel } from './pages/Hotel/Hotel';
 
-function App() {
+const App = () => {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />}></Route>
-				<Route path="/hotels" element={<List />}></Route>
-				<Route path="/hotels/:id" element={<Hotel />}></Route>
-			</Routes>
-		</BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Home />}></Route>
+			<Route path="/hotels" element={<List />}></Route>
+			<Route path="/hotels/:id" element={<Hotel />}></Route>
+		</Routes>
 	);
-}
+};
 
 export default App;
